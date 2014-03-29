586bb8122f96dc6fc3f684f719a0c147481cdf28
Patrick Dobson <pjdobson@uwaterloo.ca>
2014-03-28 23:42:26 -0400

    Removed some silly stuff Moved files around some

---
+++ src/test_1.lua
@@ -0,0 +1,138 @@
+
+
+spec1 = color.constant({0.5, 0.7, 0.5})
+spec2 = color.constant({0.5, 0.4, 0.8})
+
+diff1 = color.constant({0.2, 0.2, 0.2})
+diff2 = color.constant({0.5, 0.5, 0.5})
+diff3 = color.constant({1.0, 0.6, 0.1})
+diff4 = color.constant({0.7, 0.6, 1.0})
+diff5 = color.constant({1.0,1.0,1.0})
+
+tex1 = color.texture("bump.png")
+tex2 = color.texture("earth.png")
+
+mat1 = material.new()
+mat1:set_diffuse(diff1)
+--mat1:set_bump(tex1)
+mat1:set_specular(tex2, 25)
+mat1:set_reflection(0.9)
+-- TODO - specular
+
+mat2 = material.new()
+mat2:set_diffuse(tex2)
+mat2:set_specular(spec1, 25)
+
+mat3 = material.new()
+mat3:set_diffuse(diff3)
+mat3:set_specular(spec1, 25)
+
+mat4 = material.new()
+mat4:set_diffuse(diff4)
+mat4:set_specular(spec2, 25)
+
+mat5 = material.new()
+mat5:set_diffuse(diff5)
+mat5:set_refraction(1.5, false)
+
+scene_root = scene.node("root")
+
+s1 = scene.sphere("s1", {0, 0, -400}, 100)
+--scene_root:add_child(s1)
+s1:set_material(mat1)
+
+s2 = scene.sphere("s2", {0, 0, 0}, 150)
+--scene_root:add_child(s2)
+s2:translate({200, 50, -100})
+s2:set_material(mat1)
+
+s3 = scene.sphere("s3", {0, -1200, -500}, 1000)
+--scene_root:add_child(s3)
+s3:set_material(mat2)
+
+s4 = scene.sphere("s4", {-100, 25, -300}, 50)
+--scene_root:add_child(s4)
+s4:set_material(mat3)
+
+s5 = scene.sphere("s5", {0, 100, -250}, 25)
+--scene_root:add_child(s5)
+s5:set_material(mat4)
+
+t1 = scene.torus("t1", 100, 25)
+--scene_root:add_child(t1)
+t1:translate({0,0, 300})
+--t1:rotate('y', 0.05)
+t1:set_material(mat2)
+
+csg_s1 = scene.sphere("csg_s1", {-200, 0, 0}, 100)
+csg_s1:set_material(mat2)
+
+csg_s2 = scene.torus("csg_s2", 100, 67)
+csg_s2:translate({-200, 0, 0})
+csg_s2:rotate('x', 90)
+csg_s2:rotate('y', 4)
+csg_s2:set_material(mat1)
+
+lens = scene.torus("lens", 100, 30)
+--scene_root:add_child(lens)
+lens:set_material(mat3)
+lens:translate({0,0,0})
+lens:scale({1,1,1})
+--lens:scale({1,0.1,1})
+
+csg = scene.csg("csg1", 'd', csg_s1, csg_s2)
+--scene_root:add_child(csg)
+csg:rotate('y', -45)
+
+plane = scene.mesh("mesh", {
+			{-250, -250,  0},
+			{-250,  250,  0},
+			{ 250,  250,  0},
+			{ 250, -250,  0}
+		}, {
+			{0, 1},
+			{0, 0},
+			{1, 0},
+			{1, 1}
+		}, {
+			{3,2,1,0}
+		})
+scene_root:add_child(plane)
+plane:set_material(mat2)
+
+white_light = scene.light("white_light");
+scene_root:add_child(white_light)
+white_light:light_position({-100, 150, 400})
+white_light:light_color({0.9, 0.9, 0.9})
+white_light:light_falloff({1, 0, 0})
+
+orange_light = scene.light("orange_light");
+scene_root:add_child(orange_light)
+orange_light:light_position({400, 100, 150})
+orange_light:light_color({0.7, 0.0, 0.7})
+orange_light:light_falloff({1, 0, 0})
+
+cam1 = scene.camera("cam1", 1)
+scene_root:add_child(cam1)
+cam1:cam_position({0, 0, 800}, false)
+cam1:cam_up({0, 1, 0}, false)
+cam1:cam_view({0, 0, -1}, false)
+cam1:cam_fov(50)
+
+for i=0,100 do
+	s2:rotate('y', 1)
+	s1:rotate('y', 1)
+
+	-- 'b' for BruteForce
+	rt = tracer.new(scene_root, 'b')
+	rt:set_threads(8)
+	rt:set_ambient({0.3, 0.3, 0.3})
+	
+	-- Camera ID, Output, width, height
+	rt:render(1, "test"..i..".png", 500, 500)
+
+	t1:rotate('y', 3)
+	csg:rotate('y', 5)
+	plane:rotate('y', 5)
+	plane:scale({0.98, 0.98, 1})
+end
